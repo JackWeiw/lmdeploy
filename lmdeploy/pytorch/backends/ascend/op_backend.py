c1f7593cdd74a4f907183351eaa4c82fb6416b5c
JackWeiw <51255903105@stu.ecnu.edu.cn>
2024-10-08 16:20:00 +0800

    update attention mask setting for multi conv

--- lmdeploy/pytorch/backends/ascend/op_backend.py
+++ lmdeploy/pytorch/backends/ascend/op_backend.py
@@ -78,71 +78,69 @@ class AscendOpsBackend(DefaultOpsBackend):
     def update_step_context(cls, step_context):
         """update step context."""
         kv_start_indices, attention_mask, block_table_atb = [], [], []
-        _, block_size, _ = step_context.kv_caches[0][0].shape
+        block_num, block_size, _ = step_context.kv_caches[0][0].shape
         device = step_context.block_offsets.device
 
         is_unpaged_prefill = False
         q_start_loc_cpu = step_context.q_start_loc.cpu()
-        q_seqlens_cpu = step_context.q_seqlens.cpu()
+        q_seqlens_cpu = step_context.q_seqlens.cpu().to(torch.int32)
         kv_seqlens_cpu = step_context.kv_seqlens.cpu()
         max_q_seq_len = torch.max(q_seqlens_cpu).item()
         max_kv_seq_len = torch.max(kv_seqlens_cpu).item()
-
         mask_dtype =  step_context.kv_caches[0][0].dtype
+        
         if not step_context.is_decoding:
+            print("##########mask_dtype", mask_dtype)
             is_unpaged_prefill = \
                 all((step_context.q_seqlens ==
                      step_context.kv_seqlens).tolist())
             if is_unpaged_prefill:
-                for i in range(step_context.q_start_loc.size(0)):
-                    single_attention_mask = torch.logical_not(
-                        torch.tril(
-                            torch.ones(max_q_seq_len,
-                                    max_kv_seq_len,
-                                    dtype=mask_dtype).cuda(),
-                            diagonal=max_kv_seq_len - max_q_seq_len,
-                        ))
-                    attention_mask.append(single_attention_mask)
+                attention_mask = torch.logical_not(torch.tril(torch.ones(step_context.q_start_loc.size(0), max_q_seq_len, max_kv_seq_len, dtype=torch.bool).cuda(), diagonal=0)).to(mask_dtype)
+                print("####### zheli ", attention_mask.dtype)
+        total_slots = torch.arange(block_num * block_size,
+                                   dtype=torch.long,
+                                   device=device)
+        total_slots = total_slots.view(block_num, block_size)                    
         for i in range(step_context.q_start_loc.size(0)):
             q_seq_len = int(step_context.q_seqlens[i])
             kv_seq_len = int(step_context.kv_seqlens[i])
             if not (step_context.is_decoding or is_unpaged_prefill):
-                # import pdb; pdb.set_trace()
-                print("##########mask_dtype", mask_dtype)
-                # single_attention_mask = torch.cat([torch.zeros(1, step_context.q_seqlens[i], dtype = mask_dtype),
-                #     torch.ones(1, step_context.kv_seqlens[i] - step_context.q_seqlens[i], dtype = mask_dtype)], dim = 1).cuda()
+                # 第二轮 mask 的形状为 (batch, 1, max_kv_seq_len)
+                # single_attention_mask = torch.cat([torch.zeros(1, step_context.kv_seqlens[i] - step_context.q_seqlens[i], dtype = mask_dtype),
+                #     torch.ones(1, step_context.q_seqlens[i], dtype = mask_dtype)], dim = 1).cuda()
+                # attention_mask.append(single_attention_mask)
+                
+                # # 第二轮 mask 的形状为 (numtokens, 1, max_kv_seq_len) 效果较好！！！
                 single_attention_mask = torch.logical_not(
                     torch.tril(
                         torch.ones(max_q_seq_len,
                                    max_kv_seq_len,
-                                   dtype=mask_dtype).cuda(),
-                        diagonal=step_context.kv_seqlens[i] -
-                        step_context.q_seqlens[i],
+                                   dtype=torch.bool).cuda(),
+                        diagonal=kv_seqlens_cpu[i] -
+                        q_seqlens_cpu[i],
                     ))
-                # attention_mask.append(single_attention_mask)
                 attention_mask.append(single_attention_mask.unsqueeze(1))
+                
             history_length = kv_seq_len - q_seq_len
-            block_idx = history_length // block_size
-            block_loc = step_context.block_offsets[i][block_idx]
-            token_loc = history_length % block_size
-            for j in range(q_seq_len):
-                kv_start_indices.append([block_loc * block_size + token_loc])
-                if j == q_seq_len - 1:
-                    break
-                token_loc = (token_loc + 1) % block_size
-                block_idx = block_idx if token_loc else block_idx + 1
-                block_loc = step_context.block_offsets[i][block_idx]
-        kv_start_indices = torch.tensor(kv_start_indices, device=device)
+            slot_tables = total_slots[step_context.block_offsets[i]].flatten()
+            slot_indices = [p for p in range(history_length, kv_seq_len)]
+            slots = slot_tables[slot_indices].reshape((-1, 1))
+            kv_start_indices.append(slots)            
+
+        kv_start_indices = torch.cat(kv_start_indices).flatten()
         
         # if not (step_context.is_decoding or is_unpaged_prefill):
         block_table_atb = torch.cat([step_context.block_offsets[i].repeat(q_seqlens_cpu[i], 1) for i in range(q_seqlens_cpu.shape[0])], dim=0).to(torch.int32)
         kv_seq_len_atb = torch.cat([kv_seqlens_cpu[i].repeat(q_seqlens_cpu[i]) for i in range(q_seqlens_cpu.shape[0])], dim = 0).to(torch.int32)
-        # else:
-        #     block_table_atb = step_context.block_offsets
-        #     kv_seq_len_atb = kv_seqlens_cpu
 
         if len(attention_mask) > 0:
-            attention_mask = torch.stack(attention_mask, dim = 0)
+            if not (step_context.is_decoding or is_unpaged_prefill):
+                # # 第二轮 mask 的形状为 (batch, 1, max_kv_seq_len)
+                # attention_mask = torch.stack(attention_mask, dim = 0).to(mask_dtype)
+                # 第二轮 mask 的形状为 (numtokens, 1, max_kv_seq_len)
+                attention_mask = torch.cat(attention_mask, dim = 0).to(mask_dtype)
+  
+                
         attn_meta_cls = cls.get_attention_metadata_cls()
         attn_metadata = attn_meta_cls(
             step_context.is_decoding,
